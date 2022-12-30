bd2be1b40b2b15a283e492321d5e2cb5ad42c0ae
Georgi93an <georgiantoliev@gmail.com>
2022-12-30 13:11:01 +0100

    //1.2.3 Creation of method to generate and load accounts

--- GANBank/src/mainpackage/MainClass.java
+++ GANBank/src/mainpackage/MainClass.java
@@ -8,13 +8,20 @@ public class MainClass {
 
 	//1.1.2 Creation of Main class and methods to load clients	
 
-	//1.1.2	
+	//1.1.2	Declaring the collection for clients
 	private static Collection<Client>clients= new ArrayList<>();	
-
+	
+	//1.2.3 Declaring the collection of accounts
+	private static Collection<Account>accounts= new ArrayList<>();
+	
 	public static void main(String[] args) {
 		//1.1.2
 		generateClients(3);
 		displayClients(clients);
+		
+		//1.2.3
+		generateAccounts(clients);
+		displayAccounts(accounts);
 	}
 
 	//1.1.2 Method to generate clients
@@ -30,4 +37,21 @@ public class MainClass {
 		System.out.println("Clients"+"\n"+"**********");
 		System.out.println(clientsToString);
 	}
+	
+	//1.2.3 Method to generate accounts
+	private static void generateAccounts(Collection<Client>clients) {
+		for (Client client : clients) {
+			accounts.add(new CurrentAccount("Label for Current Accounts", client));
+			accounts.add(new SavingsAccount("Label for Savings Accounts", client));
+		}
+	}
+	
+	//1.2.3 Method to display accounts
+	private static void displayAccounts(Collection<Account>accounts) {
+		String accountsToString=accounts.stream().map(Account::toString).collect(Collectors.joining(""));
+		System.out.println("Accounts"+"\n"+"**********");
+		System.out.println(accountsToString);	
+	}
+	
+	
 }
