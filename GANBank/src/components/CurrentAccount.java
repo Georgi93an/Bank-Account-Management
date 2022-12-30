60939fbd22f609d15f3ace7b12c911bfb20bd824
Georgi93an <georgiantoliev@gmail.com>
2022-12-30 13:03:06 +0100

    //1.2.2 Creation of Current & Savings account classes

---
+++ GANBank/src/components/CurrentAccount.java
@@ -0,0 +1,10 @@
+package components;
+
+public class CurrentAccount extends Account{
+//1.2.2 Creation of CurrentAccount class
+	
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
---
+++ GANBank/src/components/SavingsAccount.java
@@ -0,0 +1,10 @@
+package components;
+
+public class SavingsAccount extends Account{
+//1.2.2 Creation of Savings Account Class
+	
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
